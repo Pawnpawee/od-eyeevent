c967d4647723c1bc69e2a5d50ca65d2fd8334304
Kukkai <pornpawee214@gmail.com>
2026-06-18 23:30:01 +0700

    fix page view

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { kv } from '@vercel/kv'
 import HeroSection from '@/components/HeroSection'
 import RegistrationForm from '@/components/RegistrationForm'
 
-export default function Home() {
+export const dynamic = 'force-dynamic'
+
+export default async function Home() {
+  await kv.incr('pageviews')
   return (
     <main>
       <HeroSection />
